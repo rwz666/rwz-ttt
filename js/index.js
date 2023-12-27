12a4adbc89ba0091d8e428c54aa0b807c2923c82
renwuzhou <3116419886@qq.com>
2023-12-27 20:32:30 +0800

    添加浩凯专属按钮

--- js/index.js
+++ js/index.js
@@ -2,3 +2,6 @@
 document.querySelector(".btn-heart").addEventListener('click', function () {
   location.href = '/heart/heart01.html'
 })
+document.querySelector('.btn-heart-zhk').addEventListener('click', function () {
+  location.href = '/heart/heart01-zhk.html'
+})
